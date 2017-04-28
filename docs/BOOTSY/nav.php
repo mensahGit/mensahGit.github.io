cd19350b5c6f94e5244fac8d897fe78d229b65dd
mensahGit <mensah@m.local>
2017-04-28 03:38:34 -0300

    newly added docs/

---
+++ docs/BOOTSY/nav.php
@@ -0,0 +1,219 @@
+
+<div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Project Mensah</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+
+
+
+                <li class="dropdown">
+                  <a href="software.html" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Software <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="sourcecode.html">Photoshop</a></li>
+                    <li><a href="resources.html">Illustrator</a></li>
+                    <li><a href="lab.html">Blender</a></li>
+                    <li><a href="languages.html">InDesign</a></li>
+                    <li><a href="mymastermind.html">AfterEffects</a></li>
+                    <li><a href="writing.html">Premiere</a></li>
+                    <li><a href="photography.html">Substance Painter</a></li>
+                    <li><a href="index.html">Ableton Live</a></li>
+                  </ul>
+                </li>
+
+
+                <li class="dropdown">
+                  <a href="sourcecode.html" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SourceCode<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">GitHub</a></li>
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                  </ul>
+                </li>
+
+
+                <li class="dropdown">
+                  <a href="resources.html" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Resources<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Templates</a></li>
+                    <li><a href="#">Snippets</a></li>
+                    <li><a href="#">Layouts</a></li>
+                    <li><a href="#">#</a></li>
+                  </ul>
+                </li>
+
+
+                <li class="dropdown">
+                  <a href="languages.html" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Languages<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Japanese</a></li>
+                    <li><a href="#">Spanish</a></li>
+                    <li><a href="#">English</a></li>
+                    <li><a href="#">German</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li class="dropdown-header">Persons Of Interest</li>
+                    <li><a href="#">Noam Chomsky</a></li>
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                  </ul>
+                </li>
+
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ideas<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Apps</a></li>
+                    <li><a href="#">Flowcharts</a></li>
+                    <li><a href="#">Inventions</a></li>
+                    <li><a href="#">#</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Programming<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">HTML</a></li>
+                    <li><a href="#">ACTIONSCRIPT</a></li>
+                    <li><a href="#">JAVASCRIPT</a></li>
+                    <li><a href="#">CSS</a></li>
+                    <li><a href="#">XML</a></li>
+                    <li><a href="#">JSON</a></li>
+                    <li><a href="#">PHP</a></li>
+                    <li><a href="#">SWIFT</a></li>
+                    <li><a href="#">JQUERY</a></li>
+                    <li><a href="#">MYSQL</a></li>
+                    <li><a href="#">NODEJS</a></li>
+                    <li><a href="#">PROCESSING</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Projects<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Project#0001</a></li>
+                    <li><a href="#">Project#0002</a></li>
+                    <li><a href="#">Project#0003</a></li>
+                    <li><a href="#">Project#0004</a></li>
+                    <li><a href="#">Project#0005</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Writing<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">My Blender Bible</a></li>
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li class="dropdown-header">Books</li>
+                    <li><a href="#">MyThoughts:Japan</a></li>
+                    <li><a href="#">MyThoughts:Argentina</a></li>
+                    <li><a href="#">MyThoughts:South Korea</a></li>
+                    <li><a href="#">MyThoughts:Chile</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Health<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Grub</a></li>
+                    <li><a href="#">Recipes</a></li>
+                    <li><a href="#">MealShotz</a></li>
+                    <li><a href="#">#</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li class="dropdown-header">Food-Related Projects</li>
+                    <li><a href="#">Project#0001</a></li>
+                    <li><a href="#">Project#0002</a></li>
+                    <li><a href="#">Project#0003</a></li>
+                    <li><a href="#">Project#0004</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Lab<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Workflows</a></li>
+                    <li><a href="#">Process</a></li>
+                    <li><a href="#">Pipelines</a></li>
+                    <li><a href="#">#</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Countries<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Japan</a></li>
+                    <li><a href="#">Argentina</a></li>
+                    <li><a href="#">USA:Chicago</a></li>
+                    <li><a href="#">South Korea</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Photography<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li class="dropdown-header">Projects</li>
+                    <li><a href="#">Project#0001</a></li>
+                    <li><a href="#">Project#0002</a></li>
+                    <li><a href="#">Project#0003</a></li>
+                    <li><a href="#">Project#0004</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Consciousness<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Scientific Method</a></li>
+                    <li><a href="#">God</a></li>
+                    <li><a href="#">Yoga</a></li>
+                    <li><a href="#">TaiChi</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">MyMasterMind<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">TheList</a></li>
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                    <li><a href="#">#</a></li>
+                  </ul>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mathematics<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Discrete</a></li>
+                    <li><a href="#">Algorithms</a></li>
+                    <li><a href="#">Base10</a></li>
+                    <li><a href="#">Linear Regression</a></li>
+                    <li><a href="#">Logarithms</a></li>
+                  </ul>
+                </li>
+
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
