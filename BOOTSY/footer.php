8d9c960ab16468adbf07b7059032fec36f33d20d
mensahGit <mensah@m.local>
2017-04-28 03:16:13 -0300

    appended all files

---
+++ BOOTSY/footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+ <!-- FOOTER -->
+      <footer class="footer">
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2016 MENSAHDESIGNS.COM &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+<script>
+$(document).ready(function(){
+
+<button onmouseover="document.getElementById('dropdown-menu').show()">The time is?</button>
+
+});
+</script>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="boner/docs/assets/js/vendor/jquery.min.js"><\/script>')</script> -->
+    <!--<script src="boner/docs/assets/js/bootstrap.min.js"></script> -->
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <!--<script src="boner/docs/assets/js/vendor/holder.min.js"></script> -->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!--<script src="boner/docs/assets/js/ie10-viewport-bug-workaround.js"></script> -->
+  </body>
+</html>
---
+++ BOOTSY/main.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+
+ <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="yumyum.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="catsmeow.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Heading</h2>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="soba2.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Kanji Cereal hits Market. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="soba2.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading">So, You Need A Doctor, Huh?<span class="text-muted">Good Luck Biscuit.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" src="KDDI_japSwipe.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Japan.Open For Business. <span class="text-muted">Welcome.</span></h2>
+          <p class="lead">Doctors arer nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="wireLess_superBlue.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+
+      <!-- /BEGIN PREV AND NEXT BTNS -->
+ <ul class="pager">
+  <li class="previous"><a href="#">Previous</a></li>
+  <li class="next"><a href="#">Next</a></li>
+</ul>
+
+ <ul class="pagination">
+  <li class="active"><a href="reboot.html">1</a></li>
+  <li ><a href="reboot.html">2</a></li>
+  <li><a href="reboot.html">3</a></li>
+  <li><a href="#">4</a></li>
+  <li class="disabled"><a href="#">5</a></li>
+  <li><a href="#">6</a></li>
+  <li class="active"><a href="#">7</a></li>
+  <li><a href="#">8</a></li>
+  <li><a href="#">9</a></li>
+  <li class="disabled"><a href="#">10</a></li>
+</ul>
+
+ <div class="row">
+  <div class="col-sm-4">.col-sm-4
+<h2>Rounded Corners</h2>
+  <p>The .img-rounded class adds rounded corners to an image (not available in IE8):</p>            
+  <img src="soba2.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236"> 
+  </div>
+  <div class="col-sm-4">.col-sm-4
+<h2>Rounded Corners</h2>
+  <p>The .img-rounded class adds rounded corners to an image (not available in IE8):</p>            
+  <img src="soba2.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236"> 
+  </div>
+  <div class="col-sm-4">.col-sm-4
+<h2>Rounded Corners</h2>
+  <p>The .img-rounded class adds rounded corners to an image (not available in IE8):</p>            
+  <img src="soba2.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236"> 
+  </div>
+</div>
+
+ <div class="row">
+  <div class="col-sm-4">.col-sm-4
+<h2>Circle</h2>
+  <p>The .img-circle class shapes the image to a circle (not available in IE8):</p>            
+  <img src="shadJones.jpg" class="img-circle" alt="Cinque Terre" width="304" height="236">
+  </div>
+  <div class="col-sm-4">.col-sm-4
+<h2>Circle</h2>
+  <p>The .img-circle class shapes the image to a circle (not available in IE8):</p>            
+  <img src="shadJones.jpg" class="img-circle" alt="Cinque Terre" width="304" height="236"> 
+  </div>
+  <div class="col-sm-4">.col-sm-4
+<h2>Circle</h2>
+  <p>The .img-circle class shapes the image to a circle (not available in IE8):</p>            
+  <img src="shadJones.jpg" class="img-circle" alt="Cinque Terre" width="304" height="236">
+  </div>
+</div>
+
+
+ <div class="row">
+  <div class="col-sm-4">.col-sm-4
+<h2>Thumbnail</h2>
+  <p>The .img-circle class shapes the image to a circle (not available in IE8):</p>            
+  <img src="catsmeow.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236">
+  </div>
+  <div class="col-sm-4">.col-sm-4
+<h2>Thumbnail</h2>
+  <p>The .img-circle class shapes the image to a circle (not available in IE8):</p>            
+  <img src="catsmeow.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236"> 
+  </div>
+  <div class="col-sm-4">.col-sm-4
+<h2>Thumbnail</h2>
+  <p>The .img-circle class shapes the image to a circle (not available in IE8):</p>            
+  <img src="catsmeow.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236">
+  </div>
+</div>
