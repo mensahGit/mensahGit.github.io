20872d240705da8679157b0971e1d665152c2622
mensahGit <mensah64@hotmail.com>
2019-04-12 08:58:25 -0500

    2019 Revamp

---
+++ mensah.js
@@ -0,0 +1,13 @@
+document.getElementById("logo").addEventListener("click", sayChow);
+
+function sayChow() {
+  //document.getElementById("gosh").remove();
+	
+}
+
+var logo = document.getElementById("logo").addEventListener("click", mensahFade);
+
+function mensahFade() {
+  document.getElementById("id").className ='mFade';
+	
+}
